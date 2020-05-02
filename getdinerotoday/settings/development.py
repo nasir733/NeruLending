a460847721bb8c975a04c9a677e2b5ede2fdce89
Zephaniah <langleylaiscool@gmail.com>
2020-05-02 16:45:25 -0400

    make it better

--- getdinerotoday/settings/development.py
+++ getdinerotoday/settings/development.py
@@ -10,6 +10,6 @@ DEBUG = True
 SECRET_KEY = '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki'
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'testdineroooo.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'getdinerotoday.herokuapp.com'']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
