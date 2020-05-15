24fc23becae826fbbb71ae53eef929c0a3db2b5f
Vassili <vassili@tftak.eu>
2020-05-15 15:43:36 +0300

    email fix

--- getdinerotoday/settings/production.py
+++ getdinerotoday/settings/production.py
@@ -1,7 +1,7 @@
 import dj_database_url
 from getdinerotoday.settings.settings import *
 
-DEBUG = False  # Should be false But using True to render static images from local in heroku
+DEBUG = True  # Should be false But using True to render static images from local in heroku
 
 # SECRET_KEY = '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki'
 SECRET_KEY = os.environ.get('SECRET_KEY', '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki')
@@ -16,8 +16,10 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+# EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'info@getdinerotoday.com'
 SERVER_EMAIL = EMAIL_HOST_USER
