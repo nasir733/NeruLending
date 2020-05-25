5084252aa615d1e0bf0896968a3eaa799e921330
Vassili <vassili@tftak.eu>
2020-05-25 20:07:45 +0300

    register api

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import TokenObtainPairPatchedView, GetUserByToken, FetchPersonalData
+from .views import *
 
 urlpatterns = [
 
     path('token/', TokenObtainPairPatchedView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/get_user_info/', GetUserByToken.as_view(), name='get_user_info'),
-    path('fetch_personal_data/', FetchPersonalData.as_view(), name='fetch_personal_data'),
+
+    path('user/get_user_info/', GetUserByToken.as_view(), name='get_user_info'),
+    path('user/fetch_personal_data/', FetchPersonalData.as_view(), name='fetch_personal_data'),
+    path('user/register/', RegisterNewUserAPI.as_view(), name='register_new_user'),
 
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.serializers import ModelSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
-from . import serializers
+from .serializers import TokenObtainPairPatchedSerializer, NewUserSerializer, UserDataSerializer
 from user.models import UserData, Profile
+
+
+class TokenObtainPairPatchedView(TokenObtainPairView):
+    serializer_class = TokenObtainPairPatchedSerializer
+    token_obtain_pair = TokenObtainPairView.as_view()
 
 
 class GetUserByToken(APIView):
@@ -18,24 +23,13 @@ class GetUserByToken(APIView):
         })
 
 
-class TokenObtainPairPatchedView(TokenObtainPairView):
-    serializer_class = serializers.TokenObtainPairPatchedSerializer
-    token_obtain_pair = TokenObtainPairView.as_view()
-
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = UserData
-        exclude = ['user',]
-
-
 class FetchPersonalData(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         try:
             data = UserData.objects.get(user=Profile.objects.get(user=request.user))
-            return Response(UserSerializer().to_representation(data))
+            return Response(UserDataSerializer().to_representation(data))
         except UserData.DoesNotExist:
             return Response({
                 'message': 'No data in database',
@@ -45,3 +39,22 @@ class FetchPersonalData(APIView):
                 'message': 'error',
                 'type': str(type(e)),
             })
+
+
+class RegisterNewUserAPI(generics.GenericAPIView):
+    serializer_class = NewUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        try:
+            userr = self.get_serializer(data=request.data)
+            if userr.is_valid():
+                Profile.objects.create_user(request.data['email'],
+                                            request.data['password'],
+                                            request.data['first_name'],
+                                            request.data['last_name'],
+                                            request.data['phone_number'])
+                return Response({'message': 'success'}, status=200)
+            else:
+                raise Exception("Form is not valid.")
+        except Exception as e:
+            return Response({'message': f'{e}'}, status=403)
