8f1cd5086c986010c6cf7a8068dee7c2a4253739
ziauldin123 <mziauldin2@gmail.com>
2020-08-25 01:16:40 +0500

    created dynamic link app

--- dynamic/models.py
+++ dynamic/models.py
@@ -4,12 +4,12 @@ from django.db import models
 app_name= 'dynamic'
 
 class subdomain(models.Model):
-    sub_name=models.CharField(max_length=200)
-    webinar= models.URLField(max_length=200)
-    iphoneApp= models.URLField(max_length=200)
-    androidApp= models.URLField(max_length=200)
-    chromeExt= models.URLField(max_length=200)
-    homeVideo= models.URLField(max_length=200)
+    sub_name=models.CharField(max_length=300)
+    webinar= models.URLField(max_length=300)
+    iphoneApp= models.URLField(max_length=300)
+    androidApp= models.URLField(max_length=300)
+    chromeExt= models.URLField(max_length=300)
+    homeVideo= models.URLField(max_length=300)
 
     def __str__(self):
         return self.sub_name
