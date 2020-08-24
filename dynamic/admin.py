aa3438fed0d1db27f1cd09bb461297870d36497e
ziauldin123 <mziauldin2@gmail.com>
2020-08-25 01:14:02 +0500

    created dynamic link app

--- dynamic/admin.py
+++ dynamic/admin.py
@@ -7,7 +7,6 @@ from .models import *
 app = apps.get_app_config('dynamic')
 
 class Subdomain(admin.ModelAdmin):
-	# fields = ('title','description','location','type','category','last_date','company_name','company_description','website','created_at','filled','salary','required_exp')
 	list_display = ('sub_name','webinar','iphoneApp','androidApp','chromeExt','homeVideo')
 
 
