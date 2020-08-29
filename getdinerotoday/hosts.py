8b127f6caebe3f5dc8bc4f054fbe9d1e6ab3d3cc
Zephaniah Langley <millennialbusinessbuilders@gmail.com>
2020-08-29 14:59:45 -0400

    make it better

--- getdinerotoday/hosts.py
+++ getdinerotoday/hosts.py
@@ -8,4 +8,6 @@ host_patterns = patterns(
     host(r'www', 'getdinerotoday.urls', name='www'),
     host(r'test', 'getdinerotoday.urls', name='test'),
     host(r'businesscreditbuilders', 'getdinerotoday.urls', name='businesscreditbuilders'),
+    host(r'lawyerupmycredit', 'getdinerotoday.urls', name='lawyerupmycredit'),
+    host(r'betterimagesolutions', 'getdinerotoday.urls', name='betterimagesolutions'),
 )
