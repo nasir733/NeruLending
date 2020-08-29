9ebf8df6121d3c70d57c99434bc824aed25fd2a5
Vassili <vassili@tftak.eu>
2020-08-29 21:55:32 +0300

    files services

--- dynamic/models.py
+++ dynamic/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+from services.FileServices import get_file_path
+
 # Create your models here.
 app_name = 'dynamic'
 
@@ -17,18 +19,13 @@ class Subdomain(models.Model):
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phno = PhoneNumberField(blank=True)
-    fav_icon = models.ImageField(upload_to='media', blank=True)
-    why_buy_link=models.CharField(max_length=200,blank=True)
-    appImage=models.CharField(max_length=200,blank=True)
-    primary_color=models.CharField(max_length=200,blank=True)
-    secondary_color=models.CharField(max_length=200,blank=True)
-    accent_color=models.CharField(max_length=200,blank=True)
-    bg_color=models.CharField(max_length=200,blank=True)
+    fav_icon = models.ImageField(upload_to=get_file_path, blank=True)
+    why_buy_link = models.CharField(max_length=200, blank=True)
+    appImage = models.CharField(max_length=200, blank=True)
+    primary_color = models.CharField(max_length=200, blank=True)
+    secondary_color = models.CharField(max_length=200, blank=True)
+    accent_color = models.CharField(max_length=200, blank=True)
+    bg_color = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.sub_name
-
-
-
-
-
