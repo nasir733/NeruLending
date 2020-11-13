0f4f5cd055e1d2d2a62818b5de421785c2e20da0
Vassili <vassili@tftak.eu>
2020-11-13 12:12:14 +0200

    update

--- dynamic/models.py
+++ dynamic/models.py
@@ -7,6 +7,10 @@ from user.models import Profile
 
 app_name = 'dynamic'
 
+default_front_text = "We Help Businesses Acquire Business Credit With Our Free Software, Mobile App, & Google Chrome Extension. Also, We Have A Free Personal Credit, Marketing, & Business Credit Course In Our Software As Well. Get $1,500 In Free Marketing, & Only Pay For Real Services Only When You Need It & Would Like To Upgrade & Purchase Our Business Credit Builder Package."
+default_about_text = 'Get Dinero Today offers a wide range of services. Which include getting clients loans, building business credit, $1,500 in free marketing, business credit course, autodialer, website builder and much more. Our business builder program helps you with each step of the business credit building process.'
+
+default_credit_repair_text = 'Click The Button Below To Have One On One Credit Repair <br> Start Fixing Your Credit For As Low As $49.99 Per Month'
 
 class Subdomain(models.Model):
     is_payment_done = models.BooleanField(default=False)
@@ -20,47 +24,49 @@ class Subdomain(models.Model):
     faq_page = models.CharField(max_length=300, null=True,default='https://businessbuilders.zendesk.com/hc/en-us/sections/360010349512-FAQ')
     creditRepairLink = models.CharField(max_length=300, null=True, default='/business/credit-affiliate')
     email = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, null=True)
+    favicon_title = models.CharField(max_length=200, null=True)
+    seo_description = models.CharField(max_length=200, null=True)
+    frontpage_text = models.TextField("Main text", default=default_front_text, null=True)
+    aboutus_text = models.TextField("About Us text", default=default_about_text, null=True)
+    address = models.CharField(max_length=200, null=True)
     phno = models.CharField(blank=True, max_length=100)
     logo = models.ImageField(upload_to=get_file_path)
+    favicon = models.ImageField(upload_to=get_file_path, blank=True)
     appImage = models.CharField(max_length=200, blank=True)
     primary_color = models.CharField(max_length=200, blank=True)
     secondary_color = models.CharField(max_length=200, blank=True)
     accent_color = models.CharField(max_length=200, blank=True)
     bg_color = models.CharField(max_length=200, blank=True)
-
-    basic_partnership_program_price = models.DecimalField(verbose_name="Basic Partnership Program Price Annual", max_digits=50, decimal_places=2, default=2000.00)
-    premium_partnership_program_price = models.DecimalField(verbose_name="Basic Partnership Program Price Monthly", max_digits=50, decimal_places=2, default=1000.00)
-
+    basic_partnership_program_price = models.DecimalField(verbose_name="Basic Partnership Program Price Annual",max_digits=50, decimal_places=2, default=2000.00)
+    premium_partnership_program_price = models.DecimalField(verbose_name="Basic Partnership Program Price Monthly",max_digits=50, decimal_places=2, default=1000.00)
     admins = models.ManyToManyField(Profile, related_name='portals')
-
     is_paid = models.BooleanField(default=False)
     portal_price = models.DecimalField(verbose_name="Portal price", max_digits=50, decimal_places=2, default=0)
     is_main_site = models.BooleanField(default=False)
     show_index_white_label = models.BooleanField(default=False)
-
     show_affiliate = models.BooleanField(default=False)
     affiliate_link = models.CharField(max_length=200, blank=True)
-
     show_becoming_whitelabel_partner = models.BooleanField(default=False)
-
     show_appointment = models.BooleanField(default=True)
     appointment_link = models.CharField(max_length=200, blank=True)
-
     offer_paid_whitelabel = models.BooleanField(default=False)
-
     show_whitelabel_link = models.BooleanField(default=True)
     whitelabelpartner_link = models.CharField(max_length=200, blank=True)
     whitelabel_index_video = models.CharField(max_length=200, blank=True)
-
-    show_credit_repair_plan = models.BooleanField(default=True)
-
     show_why_buy_from_us = models.BooleanField(default=True)
     why_buy_link = models.CharField(max_length=200, blank=True, default='https://www.youtube.com/embed/bM8A5BDZglk')
-
     show_money_back_guarantee = models.BooleanField(default=True)
     show_free_access_to_affiliate_program = models.BooleanField(default=True)
+
+    # Credit Repair Services
+    show_credit_repair_plan = models.BooleanField(default=True)
+    credit_repair_service_text = models.TextField(default=default_credit_repair_text, null=True)
+    credit_repair_service_image = models.CharField(max_length=200, blank=True, default='https://www.creditfirm.net/wp-content/uploads/2012/06/CreditFirm-Logo-300x182.jpg')
+    credit_repair_service_button_text = models.CharField(max_length=200, blank=True, default='Click Here To Get Your Free Consultation')
+    credit_repair_consultation_link = models.CharField(max_length=200, blank=True, default='https://shareasale.com/r.cfm?b=520260&u=2228198&m=49614&urllink=&afftrack=')
+
+
 
     def __str__(self):
         return self.sub_name
--- whitelabelpartnerportal/forms.py
+++ whitelabelpartnerportal/forms.py
@@ -7,7 +7,23 @@ class WhiteLabelForm(forms.ModelForm):
     logo = forms.ImageField(required=False)
     class Meta:
         model = Subdomain
-        exclude = ['is_payment_done', 'sub_name', 'admins']
+        exclude = [
+            'is_payment_done',
+            'sub_name',
+            'admins',
+            'show_becoming_whitelabel_partner',
+            'offer_paid_whitelabel',
+            'appImage',
+            'iphoneApp',
+            'androidApp',
+            'chromeExt',
+            'extensionVideo',
+            'affiliate_link',
+            'whitelabelpartner_link',
+            'show_free_access_to_affiliate_program',
+            'whitelabel_index_video',
+            'is_main_site',
+        ]
 
 
 
