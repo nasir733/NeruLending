88f4eaa52f60ab7a5c10baf5a1631f69f594ceb4
Vassili <vassili@tftak.eu>
2021-03-17 22:10:18 +0200

    upd hosts

--- services/HostsService.py
+++ services/HostsService.py
@@ -15,28 +15,17 @@ class HostsService:
     def get_host_patterns(cls):
         from dynamic.models import Subdomain
         hosts_from_model = []
-        has_www = False
         try:
             subdomains = Subdomain.objects.all()
             for i in subdomains:
-                if i.sub_name == 'www':
-                    has_www = True
-                hosts_from_model.append(host(i.sub_name, 'getdinerotoday.urls', name=i.sub_name))
-        except Exception as e:
+                if i.sub_name != 'www':
+                    hosts_from_model.append(host(i.sub_name, 'getdinerotoday.urls', name=i.sub_name))
+        except Exception:
             pass
 
-        # print(hosts_from_model)
-        if not has_www:
-            new_host_patterns = patterns(
-                '',
-                *hosts_from_model,
-                host(r'www', 'getdinerotoday.urls', name='www'),
-
-            )
-        else:
-            new_host_patterns = patterns(
-                '',
-                *hosts_from_model,
-            )
-
+        new_host_patterns = patterns(
+            '',
+            *hosts_from_model,
+            host(r'www', 'getdinerotoday.urls', name='www'),
+        )
         return new_host_patterns
