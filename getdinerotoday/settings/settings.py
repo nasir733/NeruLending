fbfc1ecea932ded003ed4f6d5bde3ee4cce38775
Vassili <vassili@tftak.eu>
2021-04-23 10:29:12 +0300

    fix aws

--- getdinerotoday/settings/settings.py
+++ getdinerotoday/settings/settings.py
@@ -36,6 +36,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
 
+
 DEFAULT_LOGGING['handlers']['console']['filters'] = []
 
 INSTALLED_APPS = [
