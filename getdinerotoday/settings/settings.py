1b8b2b82fd889759c26d46ec049793d4b69dc10f
nasir <engnasir733@gmail.com>
2021-07-27 14:46:54 +0500

    ready to deploy

--- getdinerotoday/settings/settings.py
+++ getdinerotoday/settings/settings.py
@@ -42,6 +42,7 @@ DEFAULT_LOGGING['handlers']['console']['filters'] = []
 INSTALLED_APPS = [
     'django_hosts',
     'rest_framework',
+     'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
