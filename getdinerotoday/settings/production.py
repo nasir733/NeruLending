ffaa2f2d10e9f661cc82560c3feecff3c9692cc9
nasir733 <engnasir733@gmail.com>
2021-08-05 23:35:16 +0500

    testing the heroku

--- getdinerotoday/settings/production.py
+++ getdinerotoday/settings/production.py
@@ -2,9 +2,10 @@ import dj_database_url
 
 from getdinerotoday.settings.settings import *
 
-DEBUG = False
+DEBUG = True
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki')
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
