42dc240edf320f368299e374ff6ddb5dcb8c4066
ParveenMalethia <prince.malethia.36@gmail.com>
2021-08-17 00:30:17 +0530

    static file fixing

--- bcbsoftwares/urls.py
+++ bcbsoftwares/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
+app_name = 'bcb'
+
 urlpatterns = [
     path('bcbvideosoftware/', views.bcbvideosoftware, name='index'),
     path('bcbprojectmanagment/', views.bcbprojectmanagment,
@@ -8,10 +11,10 @@ urlpatterns = [
     path('bcbappointment/',
          views.bcbappointmentsoftware, name="appointment"),
     path('bcbfilesharing/', views.bcbfilesharing, name='bcbfilesharingsoftware'),
-    path('bcbvideoconfrencing/', views.bcbvideoconferencing, name='bcbvideoconfrencingsoftware'),
+    path('bcbvideoconfrencing/', views.bcbvideoconferencing, name='bcbvideoconferencing'),
     path('bcbaccounting/', views.bcbaccountingsoftware, name='bcbaccountingsoftware'),
-    path('bcbtextmarketing/', views.bcbtextmarketing, name='bcbtextmarketingsoftware'),
-    path('bcbvoice/', views.bcbvoice, name='bcbvoicesoftware'),
+    path('bcbtextmarketing/', views.bcbtextmarketing, name='bcbtextmarketing'),
+    path('bcbvoice/', views.bcbvoice, name='bcbvoice'),
     path('bcbmarketingautomation/', views.bcbmarketingautomation, name='bcbmarketingautomation'),
     path('bcbwebsitebuilder/', views.bcbwebsitebuilder, name='bcbwebsitebuilder'),
     path('bcbwebhostingsoftware/', views.bcbwebhosting, name='bcbwebhosting'),
