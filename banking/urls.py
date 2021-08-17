7d7ef7d8006c124cf3039410a1e750cdf63a81b1
ParveenMalethia <prince.malethia.36@gmail.com>
2021-08-18 01:04:47 +0530

    url fix

--- banking/urls.py
+++ banking/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
   path('',views.Index,name='banking_home'),
-  path('about/',views.About,name='about'),
-  path('contact/',views.Contact,name='contact'),
-  path('faq/',views.Faq,name='faq'),
-  path('pricing/',views.Pricing,name='pricing'),
-  path('privacy-policy/',views.PrivacyPolicy,name='privacy-policy'),
-  path('services/',views.Services,name='services'),
-  path('terms-of-service/',views.TermsOfService,name='terms-of-service'),
+  path('about/',views.About,name='banking_about'),
+  path('contact/',views.Contact,name='banking_contact'),
+  path('faq/',views.Faq,name='banking_faq'),
+  path('pricing/',views.Pricing,name='banking_pricing'),
+  path('privacy-policy/',views.PrivacyPolicy,name='banking_privacy-policy'),
+  path('services/',views.Services,name='banking_services'),
+  path('terms-of-service/',views.TermsOfService,name='banking_terms-of-service'),
 ]
