c2f0562586bc873af4345975cc84781e6267073e
Thasanthan Code Thamizha <thasanthancodethamizha@Thasanthans-MacBook-Pro.local>
2021-11-03 19:56:06 +0530

    updated branch2

--- getdinerotoday/settings/production.py
+++ getdinerotoday/settings/production.py
@@ -2,7 +2,7 @@ import dj_database_url
 
 from getdinerotoday.settings.settings import *
 
-DEBUG = False
+DEBUG = True
 #test
 SECRET_KEY = os.environ.get(
     'SECRET_KEY', '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki')
