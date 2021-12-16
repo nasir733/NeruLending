539b4d571d87e3f4e221afa8cc88bff95504c993
Thasanthan-codethamizha <sivakumarthasanthan@gmail.com>
2021-12-16 20:52:30 +0530

    stripe key update

--- getdinerotoday/settings/development.py
+++ getdinerotoday/settings/development.py
@@ -5,10 +5,8 @@ config = dotenv_values(".env")
 DEBUG = True
 
 SECRET_KEY = '!nz#yq7*eo@3d*1(=z=f0jd-&uq!2j#ivns(shit7*b0d_h%ki'
-STRIPE_PUBLISHABLE_KEY = os.environ.get(
-    'STRIPE_PUBLISHABLE_KEY') if not DEBUG else config['STRIPE_PUBLISHABLE_KEY']
-STRIPE_SECRET_KEY = os.environ.get(
-    'STRIPE_SECRET_KEY') if not DEBUG else config['STRIPE_SECRET_KEY']
+STRIPE_PUBLISHABLE_KEY = "pk_live_51IsMmUKRyGPxBMS0JhipiztTS8ncN5y3c0W4lScY7ksBK0xVFY9T8k3DnVBw8JfqmxwIER6oJqCNOGQox4bZALz400WV2vlw9o"
+STRIPE_SECRET_KEY = "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"
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
     # 'SHOW_TOOLBAR_CALLBACK': True,
